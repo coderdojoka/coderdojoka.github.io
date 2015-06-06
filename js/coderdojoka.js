0a88ce05ba1553d531a1755b686e09aa59962e29
Tim Pambor <tp@pambor.de>
2015-06-06 08:55:13 +0200

    Füge Dropdown-Menü für Material hinzu

--- js/coderdojoka.js
+++ js/coderdojoka.js
@@ -5,14 +5,16 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+// jQuery to collapse the navbar on load and scroll
+function collapseNavbar() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+}
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
@@ -29,6 +31,16 @@ $(function() {
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+ 
+$('.navbar-nav > li.dropdown.hidden-xs').hover(function() {
+    $('ul.dropdown-menu', this).stop(true, true).slideDown('fast');
+    $(this).addClass('open');
+  },
+  function() {
+    $('ul.dropdown-menu', this).stop(true, true).slideUp('fast');
+    $(this).removeClass('open');
+});
+
 
 // Google Maps Scripts
 // When the window has finished loading create our google map below
