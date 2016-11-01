9bafbfcee58cedbf4c829732c9698efb9ee75879
Tim Pambor <tp@pambor.com>
2016-11-01 12:13:35 +0100

    Implement EventService to query events from CoderDojo Zen platform

--- js/angular/webApp.js
+++ js/angular/webApp.js
@@ -1,4 +1,10 @@
-var app = angular.module('webApp', []);
+var app = angular.module('webApp', ['ngResource']);
+
+app.factory('EventService', function ($resource) {
+    return $resource('https://zen.coderdojo.com/api/2.0/events/search', null, {
+	    'search': {method: 'POST', isArray: true},
+	});
+});
 
 app.run(function($rootScope) {
     $rootScope.color = 'blue';
