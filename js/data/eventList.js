4924a59040342def4019aa41bfb1e4f33c26dbb6
Mark Weinreuter <mark@weinreuter.org>
2018-05-10 09:40:37 +0200

    neue termine.

--- js/data/eventList.js
+++ js/data/eventList.js
@@ -1,81 +1,64 @@
 /* The 'eventController' in AngularJS depends on this data file. */
 
 eventList = [
-    /*
-    Expected structure for "eventList" entries:
-    (Time values must be integers.)
-    {
-        "year" : 2000,
-        "month" : 1,
-        "day" : 1,
-        "startHour" : 17,
-        "startMin" : 0,
-        "endHour" : 19,
-        "endMin" : 0,
-        "location" : "Raum",
-        "isIntroEvent" : false
-    }
-    */       
-   
-      
-      {
-        "year" : 2018,
-        "month" : 03,
-        "day" : 09,
-        "startHour" : 17,
-        "startMin" : 0,
-        "endHour" : 19,
-        "endMin" : 0,
-        "location" : "Seminarraum -118",
-        "isIntroEvent" : false
-    } ,
-      {
-        "year" : 2018,
-        "month" : 03,
-        "day" : 23,
-        "startHour" : 17,
-        "startMin" : 0,
-        "endHour" : 19,
-        "endMin" : 0,
-        "location" : "Seminarraum -118",
-        "isIntroEvent" : false
-    }    ,
-      {
-        "year" : 2018,
-        "month" : 04,
-        "day" : 06,
-        "startHour" : 17,
-        "startMin" : 0,
-        "endHour" : 19,
-        "endMin" : 0,
-        "location" : "Seminarraum -118",
-        "isIntroEvent" : false
-    } ,
-      {
-        "year" : 2018,
-        "month" : 04,
-        "day" : 20,
-        "startHour" : 17,
-        "startMin" : 0,
-        "endHour" : 19,
-        "endMin" : 0,
-        "location" : "Seminarraum -118",
-        "isIntroEvent" : false
-    } ,
-      {
-        "year" : 2018,
-        "month" : 05,
-        "day" : 04,
-        "startHour" : 17,
-        "startMin" : 0,
-        "endHour" : 19,
-        "endMin" : 0,
-        "location" : "Seminarraum -118",
-        "isIntroEvent" : false
-    }
+  /*
+  Expected structure for "eventList" entries:
+  (Time values must be integers.)
+  {
+      "year" : 2000,
+      "month" : 1,
+      "day" : 1,
+      "startHour" : 17,
+      "startMin" : 0,
+      "endHour" : 19,
+      "endMin" : 0,
+      "location" : "Raum",
+      "isIntroEvent" : false
+  }
+  */
 
-   
-    
-    
-    
+  {
+    "year": 2018,
+    "month": 04,
+    "day": 20,
+    "startHour": 17,
+    "startMin": 0,
+    "endHour": 19,
+    "endMin": 0,
+    "location": "Seminarraum -118",
+    "isIntroEvent": false
+  },
+  {
+    "year": 2018,
+    "month": 05,
+    "day": 04,
+    "startHour": 17,
+    "startMin": 0,
+    "endHour": 19,
+    "endMin": 0,
+    "location": "Seminarraum -118",
+    "isIntroEvent": false
+  },
+  {
+    "year": 2018,
+    "month": 05,
+    "day": 18,
+    "startHour": 17,
+    "startMin": 0,
+    "endHour": 19,
+    "endMin": 0,
+    "location": "Seminarraum -118",
+    "isIntroEvent": false
+  },
+  {
+    "year": 2018,
+    "month": 06,
+    "day": 01,
+    "startHour": 17,
+    "startMin": 0,
+    "endHour": 19,
+    "endMin": 0,
+    "location": "Seminarraum -118",
+    "isIntroEvent": false
+  }
 ];
