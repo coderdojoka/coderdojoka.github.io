d8ea4e561073d510e290777e94336cd62e6baa33
Mark Weinreuter <mark@weinreuter.org>
2020-01-11 14:24:53 +0100

    update termine

--- js/data/eventList.js
+++ js/data/eventList.js
@@ -19,89 +19,6 @@ eventList = [
 
 
 
-  {
-    "year": 2019,
-    "month": 10,
-    "day": 18,
-    "startHour": 17,
-    "startMin": 0,
-    "endHour": 19,
-    "endMin": 0,
-    "location": "Seminarraum -118",
-    "hasExtraInfo": true,
-    "extraInfo": "für fortgeschrittene Teilnehmer",
-    "title": "Projekt-Dojo"
-  },
-
-
-  {
-    "year": 2019,
-    "month": 10,
-    "day": 25,
-    "startHour": 17,
-    "startMin": 0,
-    "endHour": 19,
-    "endMin": 0,
-    "location": "Seminarraum -118",
-    "hasExtraInfo": true,
-    "extraInfo": "für alle Teilnehmer",
-    "title": "Reguläres Dojo"
-  },
-
-
-  {
-    "year": 2019,
-    "month": 11,
-    "day": 8,
-    "startHour": 17,
-    "startMin": 0,
-    "endHour": 19,
-    "endMin": 0,
-    "location": "Seminarraum -118",
-    "hasExtraInfo": true,
-    "extraInfo": "für alle Teilnehmer",
-    "title": "Reguläres Dojo"
-  },
-
- {
-    "year": 2019,
-    "month": 11,
-    "day": 15,
-    "startHour": 17,
-    "startMin": 0,
-    "endHour": 19,
-    "endMin": 0,
-    "location": "Seminarraum -118",
-    "hasExtraInfo": true,
-    "extraInfo": "für fortgeschrittene Teilnehmer",
-    "title": "Projekt-Dojo"
-  },
- {
-    "year": 2019,
-    "month": 11,
-    "day": 22,
-    "startHour": 17,
-    "startMin": 0,
-    "endHour": 19,
-    "endMin": 0,
-    "location": "Seminarraum -118",
-    "hasExtraInfo": true,
-    "extraInfo": "für alle Teilnehmer",
-    "title": "Reguläres Dojo"
-  },
- {
-    "year": 2019,
-    "month": 12,
-    "day": 6,
-    "startHour": 17,
-    "startMin": 0,
-    "endHour": 19,
-    "endMin": 0,
-    "location": "Seminarraum -118",
-    "hasExtraInfo": true,
-    "extraInfo": "für alle Teilnehmer",
-    "title": "Reguläres Dojo"
-  },
  {
     "year": 2019,
     "month": 12,
@@ -119,6 +36,45 @@ eventList = [
     "year": 2019,
     "month": 12,
     "day": 20,
+    "startHour": 17,
+    "startMin": 0,
+    "endHour": 19,
+    "endMin": 0,
+    "location": "Seminarraum -118",
+    "hasExtraInfo": true,
+    "extraInfo": "für alle Teilnehmer",
+    "title": "Reguläres Dojo"
+  },
+ {
+    "year": 2020,
+    "month": 1,
+    "day": 10,
+    "startHour": 17,
+    "startMin": 0,
+    "endHour": 19,
+    "endMin": 0,
+    "location": "Seminarraum -118",
+    "hasExtraInfo": true,
+    "extraInfo": "für alle Teilnehmer",
+    "title": "Reguläres Dojo"
+  },
+ {
+    "year": 2020,
+    "month": 1,
+    "day": 17,
+    "startHour": 17,
+    "startMin": 0,
+    "endHour": 19,
+    "endMin": 0,
+    "location": "Seminarraum -118",
+    "hasExtraInfo": true,
+    "extraInfo": "für fortgeschrittene Teilnehmer",
+    "title": "Projekt-Dojo"
+  },
+ {
+    "year": 2020,
+    "month": 1,
+    "day": 24,
     "startHour": 17,
     "startMin": 0,
     "endHour": 19,
